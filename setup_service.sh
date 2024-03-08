162bd79eaa4914357dbc422c0c26604aff4436c9
Diana Santavec <diana.santavec@gmail.com>
2024-03-08 18:47:20 +0100

    dir -p fixed

--- setup_service.sh
+++ setup_service.sh
@@ -11,7 +11,7 @@ sudo chmod +x /usr/bin/traceroute_device.sh
 sudo cp trace-network.service /lib/systemd/system/trace-network.service
 sudo cp trace-network.service /etc/systemd/system/trace-network.service
 
-sudo mkdir /usr/share/trace-network
+sudo mkdir -p /usr/share/trace-network
 sudo cp data/ping_devices.csv /usr/share/trace-network/ping_devices.csv
 sudo cp data/traceroute_devices.csv /usr/share/trace-network/traceroute_devices.csv
 
